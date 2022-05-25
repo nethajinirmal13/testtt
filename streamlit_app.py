93aecb1c05fb059183349746eeb6c339268c2fc5
nethaji nirmal <nethajinirmal13@gmail.com>
2022-05-25 21:12:21 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import streamlit_analytics
+
+with streamlit_analytics.track():
+    st.text_input("Write your name")
+    st.selectbox("Select your favorite", ["cat", "dog", "flower"])
+    st.button("Click me")
