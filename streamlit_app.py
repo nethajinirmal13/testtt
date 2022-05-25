9e9001107cdda9a16fca9eb0cc54ad0e324a42bb
nethaji nirmal <nethajinirmal13@gmail.com>
2022-05-25 21:25:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
 import streamlit_analytics
+import streamlit_authenticator as stauth
+
+names = ['nirmal', 'n']
+usernames = ['g', 'guvi']
+passwords = ['123', '456']
+
+
+authenticator = stauth.Authenticate(names, usernames)
 
 with streamlit_analytics.track():
     st.text_input("Write your name")
