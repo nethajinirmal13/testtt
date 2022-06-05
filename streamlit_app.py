550b06016e92789a27167c1c2df0ffcbf23a4ce6
nethaji nirmal <nethajinirmal13@gmail.com>
2022-06-05 06:47:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,4 @@ with streamlit_analytics.track():
     st.text_input("Write your name")
     st.selectbox("Select your favorite", ["guvi", "data", "science"])
     st.button("Click me")
-    if streamlit_analytics.split_test("option a", 2):
-        st.button("Is this button text better?")
-
-    if streamlit_analytics.split_test("option b", 2):
-        st.button("...or this one?")
+    
